4a3d331241095143b20edac68610f8f0a0c05ad3
mateopuli <101335430+mateopuli@users.noreply.github.com>
2022-03-31 14:31:57 -0300

    ej 8 y 12 incompleto

---
+++ Assets/Scripts/Ej_12.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//12. En un casino de juegos se desea mostrar los mensajes respectivos por el puntaje 
+//obtenido en el lanzamiento de tres dados de un cliente, de acuerdo a los siguientes  resultados:  
+//• Si los tres dados son seis, mostrar el mensaje “Excelente” 
+//• Si dos dados son seis, mostrar el mensaje “Muy bien”  
+//• Si un dado es seis, mostrar el mensaje “Regular”  
+//• Si ningún dado se obtiene seis, mostrar el mensaje “Insuficiente” 
+
+
+public class Ej_12 : MonoBehaviour
+{
+    public int dado1;
+    public int dado2;
+    public int dado3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej_8.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//8. Crear una variable entera que pueda ser modificada desde el Inspector.Suponiendo que el primer día de la semana
+//es el Domingo, mostrar el nombre del día correspondiente al valor de la variable “dia”. 
+//Si el día no está en el rango  permitido debe emitir por pantalla el mensaje de error "El día ingresado no es válido".
+
+public class Ej_8 : MonoBehaviour
+{
+    public int dia;
+    // Start is called before the first frame update
+    void Start()
+    {
+      switch (dia)
+        {
+            case 1:
+                Debug.Log("Domingo");
+                break;
+            case 2:
+                Debug.Log("Lunes");
+                break;
+            case 3:
+                Debug.Log("Martes");
+                break;
+            case 4:
+                Debug.Log("Miercoles");
+                break;
+            case 5:
+                Debug.Log("Jueves");
+                break;
+            case 6:
+                Debug.Log("Viernes");
+                break;
+            case 7:
+                Debug.Log("Sabado");
+                break;
+            default:
+                Debug.Log("El día ingresado no es válido");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
