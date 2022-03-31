855a810ee3c2491593ed92c13c927fa2c93e8a16
mateopuli <101335430+mateopuli@users.noreply.github.com>
2022-03-31 13:55:35 -0300

    actualizacion

---
+++ Assets/Scripts/Ej_6.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//6. Realizá un programa que permita el ingreso por Inspector de un valor entero mayor que 0 en una 
+//variable llamada num1 y muestre un mensaje por pantalla indicando "el  número es par" o "el número es impar".
+//Deberá utilizar el operador “módulo” es el caracter  %.
+
+public class Ej_6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
